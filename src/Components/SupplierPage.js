0313f94a25257c15c622a987cea1dc843476461c
Pritam Agrawal <pritam@Pritams-MacBook-Pro.local>
2021-01-29 17:29:19 +0530

    Linked Supplier page to ItemsPage

--- src/Components/SupplierPage.js
+++ src/Components/SupplierPage.js
@@ -18,21 +18,21 @@ class SupplierPage extends Component{
     render() {
         const SupplierFiltered = Suppliers.filter(Supplier => Supplier.Name === this.state.selectedOption)[0]
         return (
-            <div>
-                <DropdownButton title={this.state.selectedOption}>
-                    {Suppliers.map((supplier) => (
-                        <Dropdown.Item key={supplier.id} eventKey={supplier.Name} onSelect={this.handleSelect}>{supplier.Name}</Dropdown.Item>
-                    ))}
-                </DropdownButton>
+            <div className="d-flex justify-content-left">
                 <div>
-                    <p><strong>Supplier Details:</strong></p>
-                    <p>
+                    <h3><strong>Supplier Details:</strong></h3>
+                    <DropdownButton title={this.state.selectedOption}>
+                        {Suppliers.map((supplier) => (
+                            <Dropdown.Item key={supplier.id} eventKey={supplier.Name} onSelect={this.handleSelect}>{supplier.Name}</Dropdown.Item>
+                        ))}
+                    </DropdownButton>
+                    <h5>
                         <strong>Name: </strong><em>{SupplierFiltered.Name}</em> <br />
                         <strong>Address: </strong><em>{SupplierFiltered.Address}</em><br />
                         <strong>Phone: </strong><em>{SupplierFiltered.Phone}</em><br />
                         <strong>Email: </strong><em>{SupplierFiltered.Email}</em><br/>
                         <strong>Website: </strong><em>{SupplierFiltered.Website}</em>
-                    </p>
+                    </h5>
                 </div>
                 
             </div>
